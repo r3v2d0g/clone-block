c08616811e4ca68b6af44559084d3ced3cd5a7ad
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-08-30 17:22:03 +0200

    Fix documentation and bump version

--- src/lib.rs
+++ src/lib.rs
@@ -8,26 +8,28 @@
 
 // ======================================== Documentation ======================================= \\
 
-/// A very simple macro that clones a list of variables before calling an expression.
-///
-/// ## Example
-///
-/// ```rust
-/// use clone_block::clone;
-/// use std::thread;
-///
-/// let foo = "foo".to_string();
-///
-/// let thread = thread::spawn(
-///     clone!(foo; move || {
-///         let foobar = format!("{}bar", foo);
-///         foobar
-///     })
-/// );
-///
-/// let foobar = thread.join();
-/// let foobaz = format!("{}baz", foo);
-/// ```
+//! A very simple macro that clones a list of variables before calling an expression.
+//!
+//! Based on this tweet: <https://twitter.com/untitaker/status/1299812136202493953>
+//!
+//! ## Example
+//!
+//! ```rust
+//! use clone_block::clone;
+//! use std::thread;
+//!
+//! let foo = "foo".to_string();
+//!
+//! let thread = thread::spawn(
+//!     clone!(foo; move || {
+//!         let foobar = format!("{}bar", foo);
+//!         foobar
+//!     })
+//! );
+//!
+//! let foobar = thread.join();
+//! let foobaz = format!("{}baz", foo);
+//! ```
 
 // ======================================== macro_rules! ======================================== \\
 
