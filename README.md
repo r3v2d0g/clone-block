de092af0f3f903117232689feb23d9e4b38dcb6e
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-08-30 17:16:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# A very simple macro that clones a list of variables before calling an expression
+
+[![img](https://img.shields.io/crates/l/clone-block.svg)](https://github.com/r3v2d0g/clone-block/blob/main/LICENSE.txt) [![img](https://img.shields.io/crates/v/clone-block.svg)](https://crates.io/crates/clone-block) [![img](https://docs.rs/clone-block/badge.svg)](https://docs.rs/clone-block)
+
+```rust
+use clone_block::clone;
+use std::thread;
+
+let foo = "foo".to_string();
+
+let thread = thread::spawn(
+    clone!(foo; move || {
+        let foobar = format!("{}bar", foo);
+        foobar
+    })
+);
+
+let foobar = thread.join();
+let foobaz = format!("{}baz", foo);
+```
+
+
+## License
+
+> This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at <http://mozilla.org/MPL/2.0/>.
---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+/**************************************************************************************************
+ *                                                                                                *
+ * This Source Code Form is subject to the terms of the Mozilla Public                            *
+ * License, v. 2.0. If a copy of the MPL was not distributed with this                            *
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.                                       *
+ *                                                                                                *
+ **************************************************************************************************/
+
+// ======================================== Documentation ======================================= \\
+
+/// A very simple macro that clones a list of variables before calling an expression.
+///
+/// ## Example
+///
+/// ```rust
+/// use clone_block::clone;
+/// use std::thread;
+///
+/// let foo = "foo".to_string();
+///
+/// let thread = thread::spawn(
+///     clone!(foo; move || {
+///         let foobar = format!("{}bar", foo);
+///         foobar
+///     })
+/// );
+///
+/// let foobar = thread.join();
+/// let foobaz = format!("{}baz", foo);
+/// ```
+
+// ======================================== macro_rules! ======================================== \\
+
+#[macro_export]
+/// ## Example
+///
+/// ```rust
+/// use clone_block::clone;
+/// use std::thread;
+///
+/// let foo = "foo".to_string();
+///
+/// let thread = thread::spawn(
+///     clone!(foo; move || {
+///         let foobar = format!("{}bar", foo);
+///         foobar
+///     })
+/// );
+///
+/// let foobar = thread.join();
+/// let foobaz = format!("{}baz", foo);
+/// ```
+macro_rules! clone {
+    ($($var:ident),+ ; $expr:expr) => {{
+        $(let $var = $var.clone();)+
+
+        $expr
+    }};
+}
